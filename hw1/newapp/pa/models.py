86a3ba722a2664c9b7159c24d97637bbc3c98eb6
karimdad <karimdadniaz@gmail.com>
2023-04-02 12:28:15 +0500

    removed unncessary projects

--- hw1/newapp/pa/models.py
+++ hw1/newapp/pa/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+def User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+def Project(models.Model):
+    title = models.CharField(max_length=100)
+    created_by = models.CharField(max_length=100)
